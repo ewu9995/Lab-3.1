61591f1484eeb2d9a7d6beef7e84c6d22b5f4839
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-18 09:23:14 -0500

    sdwqeqw

--- Lab 3.1/src/ArrayMethods1.java
+++ Lab 3.1/src/ArrayMethods1.java
@@ -1,6 +1,14 @@
 
 public class ArrayMethods1 {
 	
+	public static void main(String[] args) {
+		int[] list1 = {2,4,6,7,8,3,1};
+		bubbleSort(args);
+		Systen.out.println
+	}
+		
+	
+	
 	
 	public static void insertionSort(int [] list1) {
 		
@@ -14,11 +22,39 @@ public class ArrayMethods1 {
 	
 	public static void bubbleSort(String [] list1) {
 		
-	}
+		for(int i = 0; i < list1.length-1; i++) {
+			int a = Integer.parseInt(list1[i]);  
+			int b = Integer.parseInt(list1[i+1]);	
+			boolean c = a>b;
+			if(c) {
+				bubbleSwap(list1, a, b);
+				}
+			}
+		}
+	
 	
 	public static void swap(int []arr, int index1, int index2) {
 		int temp = arr[index1];
 		arr[index1] = arr[index2];
 		arr[index2] = temp;
 	}
+	
+	public static void bubbleSwap(String []arr, int index1, int index2) {
+		String temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void selectionSwap(double []arr, int index1, int index2) {
+		double temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
 }
+
+
+
+
+
+
+
